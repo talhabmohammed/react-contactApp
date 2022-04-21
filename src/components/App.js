4c93708822e68653a99c9a02a01c9593293aaca1
talhabmohammed <79557504+talhabmohammed@users.noreply.github.com>
2022-04-21 18:26:09 -0500

    First Commit

---
+++ src/components/App.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { v4 as uuidv4 } from "uuid";
+import "./App.css";
+import Header from "./Header";
+import AddContact from "./AddContact";
+import ContactList from "./ContactList";
+
+function App() {
+  const LOCAL_STORAGE_KEY = "contacts";
+  const [contacts, setContacts] = useState([]);
+
+  const addContactHandler = (contact) => {
+    console.log(contact);
+    setContacts([...contacts, { id: uuidv4(), ...contact }]);
+  };
+
+  const removeContactHandler = (id) => {
+    const newContactList = contacts.filter((contact) => {
+      return contact.id !== id;
+    });
+
+    setContacts(newContactList);
+  };
+
+  useEffect(() => {
+    const retriveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (retriveContacts) setContacts(retriveContacts);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  }, [contacts]);
+
+  return (
+    <div className="ui container">
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<ContactList contacts={contacts} getContactId={removeContactHandler} />}></Route>
+          <Route path="/add" element={<AddContact addContactHandler={addContactHandler} />}></Route>
+        </Routes>
+        {/* <AddContact addContactHandler={addContactHandler} /> */}
+        {/* <ContactList contacts={contacts} getContactId={removeContactHandler} /> */}
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="ui large menu">
+      <div className="active item">
+        <h2>Contact Manager</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
